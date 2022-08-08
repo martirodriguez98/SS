10edaed8e2f53c48548634c61ed12230ea37661f
martina <martinrodriguez@itba.edu.ar>
2022-08-08 13:32:38 -0300

    input parser
    Co-authored-by: anita <atarantino@itba.edu.ar>
    Co-authored-by: gui <ggordyn@itba.edu.ar>

--- TP1/src/main/java/CellIndexMethod.java
+++ TP1/src/main/java/CellIndexMethod.java
@@ -1,3 +1,79 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class CellIndexMethod {
 
+    public static File cellIndexMethod(List<Particle> particles, int M, double r_c) {
+
+
+        File output = new File("TP1/src/main/resources/ArchivosEjemplo/output.txt");
+
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        //todo check if parameters are ok.
+        File static_file = new File(args[0]);
+        int N = 0;
+        double L = 0;
+        List<Particle> particles = null;
+        try {
+            Scanner myReader = new Scanner(static_file);
+            int index = 0;
+            particles = new LinkedList<>();
+            while (myReader.hasNextLine()) {
+                if (index == 0) {
+                    N = Integer.parseInt(myReader.nextLine());
+                } else if (index == 1) {
+                    L = Double.parseDouble(myReader.nextLine());
+                } else {
+                    String data = myReader.nextLine();
+                    String[] tokens = data.split("\s+");
+                    Particle p = new Particle(Double.parseDouble(tokens[0]), Double.parseDouble(tokens[1]), index - 1);
+                    particles.add(p);
+                }
+                index++;
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        if(particles != null) {
+            File dynamic_file = new File(args[1]);
+            try {
+                int index = 0;
+                Scanner myReader = new Scanner(dynamic_file);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    String[] tokens = data.split("\s+");
+                    if (tokens.length == 1) {
+                        //time
+                        index = 0;
+                    } else {
+                        particles.get(index-1).setPosition(new Position(Double.parseDouble(tokens[0]), Double.parseDouble(tokens[1])));
+                    }
+                    index++;
+
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+
+        double rc = Double.parseDouble(args[2]);
+
+        assert particles != null;
+        double max_radio = particles.stream().max(Comparator.comparing(Particle::getRadio)).get().getRadio();
+
+        int M = (int) Math.floor(L/(rc + 2 * max_radio));
+
+        cellIndexMethod(particles,M, rc);
+
+    }
 }
--- TP1/src/main/java/Particle.java
+++ TP1/src/main/java/Particle.java
@@ -1,4 +1,46 @@
 public class Particle {
-    private Position position;
     private double radio;
+    private double property;
+    private long id;
+    private Position position; //if we need the time in the future, we save it as HashMap<time,position>
+
+    public Particle(double radio, double property, long id) {
+        this.id = id;
+        this.radio = radio;
+        this.property = property;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getProperty() {
+        return property;
+    }
+
+    public void setProperty(double property) {
+        this.property = property;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Particle{" +
+                "radio=" + radio +
+                ", property=" + property +
+                ", id=" + id +
+                ", position=" + position +
+                '}';
+    }
 }
